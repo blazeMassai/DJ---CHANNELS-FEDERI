743107f51edec0976a6fa13b7322fe67b39da14c
blazemassai <jayblaze291@gmail.com>
2022-04-28 14:41:33 +0300

    contactform

--- 1_PROJECT_BOOK/booktime/main/tests/test_views.py
+++ 1_PROJECT_BOOK/booktime/main/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from main import forms
 
 # Create your tests here.
 
@@ -15,3 +16,11 @@ class TestPage(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about_us.html')
         self.assertContains(response, 'BookTime')
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(reverse("contact-us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact_form.html')
+        self.assertContains(response, 'BookTime')
+        self.assertIsinstance(response.context["form"], forms.ContactForm
+                              )
--- 1_PROJECT_BOOK/booktime/main/urls.py
+++ 1_PROJECT_BOOK/booktime/main/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
         path('about-us', TemplateView.as_view(template_name="about_us.html"), name='about-us'),
         path('', TemplateView.as_view(template_name="home.html"), name='home'),
+        path("contact-us/",views.ContactUsView.as_view(), name='contact-us')
 ]
