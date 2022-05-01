2716534254c98d84aa4e171ab132b20c345bf884
unknown <reward.masalla@trc.co.tz>
2022-05-02 00:49:43 +0300

    page 119 prod detail

--- 1_PROJECT_BOOK/booktime/main/urls.py
+++ 1_PROJECT_BOOK/booktime/main/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from . import views
+from main import models
 
 urlpatterns = [
         path('about-us', TemplateView.as_view(template_name="about_us.html"), name='about-us'),
         path('', TemplateView.as_view(template_name="home.html"), name='home'),
         path("contact-us/",views.ContactUsView.as_view(), name='contact-us'),
         path("products/<slug:tag>/",views.ProductListView.as_view(),name="products"),
+        path("product/<slug:slug>/",DetailView.as_view(model=models.Product),name="product"),
 ]
